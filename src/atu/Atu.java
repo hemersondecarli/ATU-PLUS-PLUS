2bcf938f12acee97e13be889f0be1bfd195d2e90
Hemerson <>
2024-02-08 22:36:27 +0000

    firstly I installed netbeans 8.2 because the other ones were not working for me.
    I started by creating a project, and then creating my first Jframe form where im going to design and program the login page.
    on that page I also made the connection with the database on my localhost.
    
    I created a simple database to start where i added user and password, just to certify the connection was working fine.

---
+++ src/atu/Atu.java
@@ -0,0 +1,16 @@
+package atu;
+
+/**
+ *
+ * @author alber
+ */
+public class Atu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+    
+}
